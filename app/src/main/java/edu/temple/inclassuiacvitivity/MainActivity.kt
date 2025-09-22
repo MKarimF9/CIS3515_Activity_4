aaf347d25943bab2d4e8cd8b795a089f1bb3c931
Mohammed Karim <makarim913@temple.edu>
2025-09-22 18:09:13 -0400

    class activity

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
+
+// TODO Replace ArrayAdapter with your implementation of TextSizeAdapter
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,30 +16,28 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
-        /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        val numberArray = (1..20).map { it * 5 }.toTypedArray()
+        /* Step 1: Populate this array */
+        val numberArray = Array(50){i -> (i + 1) * 2}
 
-        println(numberArray)
+        with (spinner) {
+            /* Step 2: Create adapter to display items from array in Spinner */
+            //adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, numberArray)
+            adapter = TextSizeAdapter(this@MainActivity, numberArray)
 
-        /* TODO Step 3: Create adapter to display items from array in Spinner */
-        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, numberArray)
 
+            // Step 3: Change TextView's text size to the number selected in the Spinner */
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    parent?.run {
+                        displayTextView.textSize = getItemAtPosition(position).toString().toFloat()
 
-        // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                parent?.run {
-                displayTextView.textSize = getItemAtPosition(position).toString().toFloat()
+                    }
                 }
 
-            }
+                override fun onNothingSelected(parent: AdapterView<*>?) {
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
             }
         }
 
